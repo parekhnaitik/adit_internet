621299c68543c6e390eda21a33c43b666ea42974
Naitik Parekh <parekhnaitik13@gmail.com>
2017-11-19 16:35:16 +0530

    database connection tested

---
+++ src/in/ac/adit/internet/bean/InternetUser.java
@@ -0,0 +1,60 @@
+package in.ac.adit.internet.bean;
+
+public class InternetUser {
+	String userId;
+	String firstName;
+	String lastName;
+	String enrollmentNumber;
+	String emailId;
+	String contactNumber;
+	String department;
+	String userType;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEnrollmentNumber() {
+		return enrollmentNumber;
+	}
+	public void setEnrollmentNumber(String enrollmentNumber) {
+		this.enrollmentNumber = enrollmentNumber;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}	
+}
---
+++ src/in/ac/adit/internet/bean/UserDevices.java
@@ -0,0 +1,64 @@
+package in.ac.adit.internet.bean;
+
+import java.util.Date;
+
+public class UserDevices {
+	int deviceId;
+	String userId;
+	Date startDate;
+	int validity;
+	Date endDate;
+	String deviceType;
+	String macAddress;
+	String ipAddress;
+	//Date date = new SimpleDateFormat("dd/MM/yyyy").parse("12/05/2015");
+	public int getDeviceId() {
+		return deviceId;
+	}
+	public void setDeviceId(int deviceId) {
+		this.deviceId = deviceId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public int getValidity() {
+		return validity;
+	}
+	public void setValidity(int validity) {
+		this.validity = validity;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public String getDeviceType() {
+		return deviceType;
+	}
+	public void setDeviceType(String deviceType) {
+		this.deviceType = deviceType;
+	}
+	public String getMacAddress() {
+		return macAddress;
+	}
+	public void setMacAddress(String macAddress) {
+		this.macAddress = macAddress;
+	}
+	public String getIpAddress() {
+		return ipAddress;
+	}
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+	
+}
